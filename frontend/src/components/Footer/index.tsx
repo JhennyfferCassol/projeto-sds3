dc7581b08e2906bcc68a267e82dae59a246dac61
Jhenny <jhennyfferemmily@hotmail.com>
2021-09-11 21:15:46 -0300

    Security config

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/jhennyffer-c-854532104/" target="_blank" rel="noreferrer">Jhennyffer Emmily Cassol</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,14 @@
-const Footer = () => {
+import ImgDsDark from 'assets/img/ds-dark.svg';
+const NavBar = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/jhennyffer-c-854532104/" target="_blank" rel="noreferrer">Jhennyffer Cassol</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <nav className="my-2 my-md-0 mr-md-3">
+                        <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                </nav>
             </div>
-        </footer>
+        </div>
     );
 }
 
-export default Footer;
+export default NavBar;
